e45e4bd26dfc3a7af55eaff76a40e689cfc5babf
anhnqph37744 <anhnqph37744@fpt.edu.vn>
2025-03-14 21:52:44 +0700

    migration DB

--- database/migrations/2025_02_13_134441_create_products_table.php
+++ database/migrations/2025_02_13_134441_create_products_table.php
@@ -13,6 +13,19 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 200);
+            $table->decimal('price', 10, 2);
+            $table->text('description')->nullable();
+            $table->string('image', 200)->nullable();
+            $table->double('weight', 8, 2)->nullable();
+            $table->integer('quantity')->default(0);
+            $table->integer('quantity_warning')->default(0);
+            $table->string('tags', 200)->nullable();
+            $table->string('sku', 100)->unique();
+            $table->foreignId('brand_id')->nullable()->constrained('brands')->nullOnDelete();
+            $table->foreignId('category_id')->nullable()->constrained('categories')->nullOnDelete();
+            $table->string('instructional_images', 255)->nullable();
+            $table->tinyInteger('active')->default(1);
             $table->timestamps();
         });
     }
